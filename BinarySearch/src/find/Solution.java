f2a04778d3afc0f2d68a2e8909a8d1b7ac546904
TejasSaraf <saraftejas12@gmail.com>
2024-10-18 18:12:59 -0700

    Binary Search

--- BinarySearch/src/find/Solution.java
+++ BinarySearch/src/find/Solution.java
@@ -3,25 +3,26 @@ package find;
 public class Solution {
 
 	public static void main(String[] args) {
-		int[] num = { -3, 0, 5, 6, 7, 22, 34, 55, 99 };
-		int target = 99;
-		int ans = binary(num, target);
+		int[] nums = { 1, 5, 6, 9, 12, 33, 54, 67, 89, 91 };
+		int target = 91;
+		int ans = binarySearch(nums, target);
 		System.out.println(ans);
 	}
 
-	public static int binary(int[] num, int target) {
+	static int binarySearch(int[] nums, int target) {
 		int start = 0;
-		int end = num.length - 1;
+		int end = nums.length - 1;
 
 		while (start <= end) {
 			int mid = start + (end - start) / 2;
 
-			if (target > num[mid]) {
-				start = mid + 1;
-			} else if (target < num[mid]) {
-				end = mid - 1;
-			} else {
+			if (target == nums[mid]) {
 				return mid;
+			}
+			if (target > nums[mid]) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
 			}
 		}
 		return -1;
