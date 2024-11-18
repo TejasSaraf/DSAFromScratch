49f41facd4cb64d06807ff895b05d96802c56d5c
TejasSaraf <saraftejas12@gmail.com>
2024-11-18 15:39:22 -0800

    Optimized logic

--- BinarySearch/src/find/SmallestLetterGreaterThanTarget.java
+++ BinarySearch/src/find/SmallestLetterGreaterThanTarget.java
@@ -11,19 +11,20 @@ public class SmallestLetterGreaterThanTarget {
 	}
 
 	public static char nextGreatestLetter(char[] letters, char target) {
-		int start = 0;
-		int end = letters.length - 1;
+		int start = 0, end = letters.length - 1, ans = 0;
 
 		while (start <= end) {
 			int mid = start + (end - start) / 2;
 
-			if (letters[mid] > target) {
+			if (letters[mid] == target) {
+				start = mid + 1;
+			} else if (letters[mid] > target) {
 				end = mid - 1;
+				ans = mid;
 			} else {
 				start = mid + 1;
 			}
 		}
-		return letters[start % letters.length];
+		return letters[ans];
 	}
-
 }
