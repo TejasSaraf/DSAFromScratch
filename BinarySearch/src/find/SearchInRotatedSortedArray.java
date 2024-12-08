85735c8be64fca113b9e625d0081155e4891397e
TejasSaraf <saraftejas12@gmail.com>
2024-12-07 17:19:15 -0800

    Search in rotated sorted array

--- BinarySearch/src/find/SearchInRotatedSortedArray.java
+++ BinarySearch/src/find/SearchInRotatedSortedArray.java
@@ -2,36 +2,35 @@
 package find;
 
 public class SearchInRotatedSortedArray {
-	public int search(int[] nums, int target) {
-        int start=0,end=nums.length-1,mid;
-        
-        while(start<=end)
-        {
-            mid=start+(end-start)/2;
-            
-            if(nums[mid]==target)
-                return mid;
-            
-            if(nums[start]<=nums[mid])
-            {
-                if(target>=nums[start] && target<=nums[mid])
-                {
-                    end=mid-1;
-                }
-                else{
-                    start=mid+1;
-                }
-            }
-            else{
-                if(target>=nums[mid] && target<=nums[end])
-                {
-                    start=mid+1;
-                }
-                else{
-                    end=mid-1;
-                }
-            }
-        }
-        return -1;
-    }
+	public static int search(int[] nums, int target) {
+		int start = 0;
+		int end = nums.length - 1;
+
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+
+			if (nums[mid] == target) {
+				return mid;
+			} else if (nums[mid] <= nums[end]) {
+				if (target <= nums[end] && target > nums[mid]) {
+					start = mid + 1;
+				} else {
+					end = mid - 1;
+				}
+			} else {
+				if (target >= nums[start] && target < nums[mid]) {
+					end = mid - 1;
+				} else {
+					start = mid + 1;
+				}
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String args[]) {
+		int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
+		int ans = search(nums, 0);
+		System.out.println(ans);
+	}
 }
