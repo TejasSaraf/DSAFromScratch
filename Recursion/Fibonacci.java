d1b0759273f7f41180eb7cebfa856f30a73fc026
TejasSaraf <saraftejas12@gmail.com>
2025-02-20 16:48:02 -0600

    modified fibinacci no function by adding base cases

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -2,20 +2,20 @@
 public class Fibonacci {
 
     public static int fibb(int n) {
-        if (n <= 1) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n < 1) {
             return n;
         }
-
         return fibb(n - 1) + fibb(n - 2);
     }
 
     public static void main(String[] args) {
-        int n = 12;
-        fibb(n);
-
-        for (int i = 0; i < n; i++) {
-            System.out.print(fibb(i) + " ");
-        }
-
+        int n = 2;
+        System.out.println(fibb(n));
     }
 }
